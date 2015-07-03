3742157e12030f5c3cb7728a5c01b5aa77e02dc7
Dzung Nguyen <ntd1712@gmail.com>
2015-07-03 10:13:50 +0700

    add FilesystemCacheProvider provider class

---
+++ src/CacheProvider/FilesystemCacheProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Atrauzzi\LaravelDoctrine\CacheProvider;
+
+use Doctrine\Common\Cache\FilesystemCache;
+
+/**
+ * Class FilesystemCacheProvider
+ * @package Atrauzzi\LaravelDoctrine\CacheProvider
+ */
+class FilesystemCacheProvider extends CacheProvider
+{
+    /** {@inheritdoc} */
+    protected function initialize($config)
+    {
+        return new FilesystemCache($config['directory'], $config['extension']);
+    }
+}
